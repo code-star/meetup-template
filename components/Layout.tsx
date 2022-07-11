83a36d7e5291941bc7cf8cacdb967f2666dcab94
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-07-11 09:41:26 +0200

    Add styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,32 @@
 import React, { ReactNode } from "react";
 import Link from "next/link";
 import Head from "next/head";
+// import fav from "../public/assets/favicon.png";
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
+const Layout = ({ children, title = "Home" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      {/* <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"
+      /> */}
+      <link
+        rel="icon"
+        href="meetup-template/assets/favicon.png"
+        // href="public/assets/favicon.png"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&display=optional"
+        rel="stylesheet"
+      />
     </Head>
     <header>
       <nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,42 +13,50 @@ const IndexPage = () => {
   const { t } = useTranslation("common");
   return (
     <Layout title={t("abstract.title")}>
-      <Image
-        src={logoImg}
-        alt="Ordina - Ahead of Change"
-        width="405"
-        height="205"
-      />
-      <h1>{t("abstract.title")}</h1>
-      <p>{t("abstract.text")}</p>
-      <a className="button" href={t("signUpUrl")}>
-        {t("abstract.buttonLabel")}
-      </a>
+      <section className="intro">
+        <div>
+          <h1>{t("abstract.title")}</h1>
+          <p>{t("abstract.text")}</p>
+          <a className="button" href={t("signUpUrl")}>
+            {t("abstract.buttonLabel")}
+          </a>
+        </div>
+        <Image
+          src={logoImg}
+          alt="Ordina - Ahead of Change"
+          width="405"
+          height="205"
+        />
+      </section>
       {/* <p>
         <Link href="/about">
           <a>About</a>
         </Link>
       </p> */}
-      <iframe
-        src={t("location.embedUrl")}
-        height="450"
-        style={{ border: 0, width: "calc(100vw - 20px)" }}
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-      <h2>{t("motivation.title")}</h2>
-      <ul>
-        <li>{t("motivation.list2.line1")}</li>
-        <li>{t("motivation.list2.line2")}</li>
-        <li>{t("motivation.list2.line3")}</li>
-        {/* <Trans i18nKey="motivation.list">
+      <section>
+        <iframe
+          src={t("location.embedUrl")}
+          height="450"
+          style={{ border: 0, width: "calc(100vw - 15px)" }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </section>
+      <section className="motivation">
+        <h2>{t("motivation.title")}</h2>
+        <ul>
+          <li>{t("motivation.list2.line1")}</li>
+          <li>{t("motivation.list2.line2")}</li>
+          <li>{t("motivation.list2.line3")}</li>
+          {/* <Trans i18nKey="motivation.list">
           <li>reason</li>
           <li>reason</li>
           <li>reason</li>
         </Trans> */}
-      </ul>
-      <a href={t("signUpUrl")}>{t("motivation.buttonLabel")}</a>
+        </ul>
+        <a href={t("signUpUrl")}>{t("motivation.buttonLabel")}</a>
+      </section>
     </Layout>
   );
 };
