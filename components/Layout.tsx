68deb67107194f29290d3edf686d89f5a05623df
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-07-11 10:01:16 +0200

    Try ico

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,15 +14,9 @@ const Layout = ({ children, title = "Home" }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      {/* <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"
-      /> */}
       <link
         rel="icon"
-        // href="meetup-template/assets/favicon.png"
-        href={`${configJson.basePath}/assets/favicon.png`}
-        // href="public/assets/favicon.png"
+        href={`${configJson.basePath}/assets/favicon.ico`}
       />
       <link
         href="https://fonts.googleapis.com/css2?family=Montserrat&display=optional"
